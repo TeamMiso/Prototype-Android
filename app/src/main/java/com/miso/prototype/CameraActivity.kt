35ad56545d211124775d63a25d23d7b0a21b70c3
GSM-Minseok <ms0818000@gmail.com>
2023-07-13 15:42:47 +0900

    이미지 저장후 로드

--- app/src/main/java/com/miso/prototype/CameraActivity.kt
+++ app/src/main/java/com/miso/prototype/CameraActivity.kt
@@ -26,6 +26,7 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.miso.prototype.databinding.ActivityCameraBinding
 import com.miso.prototype.databinding.ActivityMainBinding
+import com.miso.prototype.util.PermissionUtil
 import java.io.File
 import java.text.SimpleDateFormat
 import java.util.*
@@ -34,43 +35,60 @@ import java.util.concurrent.ExecutorService
 class CameraActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCameraBinding
     private lateinit var preview: Preview
+    private var imageCapture: ImageCapture? = null
+
+    private lateinit var outputDirectory: File
+    private lateinit var cameraExecutor: ExecutorService
+
+    private lateinit var cameraAnimationListener: Animation.AnimationListener
+
+    private var savedUri: Uri? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCameraBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setupPermissions()
-        openCamera()
+        getOutputDirectory()
+        setListener()
 
         binding.imageViewPhoto.setOnClickListener {
             startActivity(Intent(this, RecyclingActivity::class.java))
         }
     }
     private fun setupPermissions() {
-        val cameraPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
-        if (cameraPermission != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 1004)
+
+        var permissionList =
+            listOf(Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE)
+
+        if (!PermissionUtil.checkPermission(this, permissionList)) {
+            PermissionUtil.requestPermission(this, permissionList)
+        } else {
+            openCamera()
         }
     }
+
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
-        when (requestCode) {
-            1004 -> {
-                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Log.d("testt","CamOpen")
-                    openCamera()
-                } else {
-                    Toast.makeText(this, "권한허용이 필요합니다.", Toast.LENGTH_SHORT).show()
-                    onBackPressed()
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (grantResults.isNotEmpty()) {
+            for((i,permissions) in permissions.withIndex()){
+                if(grantResults[i] != PackageManager.PERMISSION_GRANTED ){
+                    Log.d("testt", "${grantResults[i]} success")
+                }else{
+                    Log.d("testt", "${grantResults[i]} fail")
                 }
             }
-            else -> {
-                super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-            }
+            openCamera()
+        } else {
+            Log.d("testt", "승인 거부")
+            onBackPressed()
         }
     }
+
     private fun openCamera() {
         Log.d("testt", "openCamera")
 
@@ -92,6 +110,45 @@ class CameraActivity : AppCompatActivity() {
                 Log.e(TAG, "Error setting up camera: ${e.message}")
             }
         }, ContextCompat.getMainExecutor(this))
+    }
+    private fun setListener() {
+        binding.imageViewPhoto.setOnClickListener {
+            savePhoto()
+        }
+    }
+    private fun savePhoto() {
+        imageCapture = imageCapture ?: return
+
+        val photoFile = File(
+            outputDirectory,
+            SimpleDateFormat("yy-mm-dd", Locale.US).format(System.currentTimeMillis()) + ".png"
+        )
+        val outputOption = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+        imageCapture?.takePicture(
+            outputOption,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    savedUri = Uri.fromFile(photoFile)
+                    Log.d("testt", "savedUri : $savedUri")
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    exception.printStackTrace()
+                    onBackPressed()
+                }
+
+            })
+
+    }
+
+    private fun getOutputDirectory(): File {
+        val mediaDir = externalMediaDirs.firstOrNull()?.let {
+            File(it, resources.getString(R.string.app_name)).apply { mkdirs() }
+        }
+        return if (mediaDir != null && mediaDir.exists())
+            mediaDir else filesDir
     }
 
 }
